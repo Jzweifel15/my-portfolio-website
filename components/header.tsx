ae42998af3e3ec883e6c4d9154c6ff688e9be244
Justin R. Zweifel <jzweifel15@yahoo.com>
2023-11-28 00:38:10 -0500

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import { motion } from "framer-motion";
+import { links } from "@/lib/data";
 
 export default function Header() {
     return (
@@ -12,6 +13,11 @@ export default function Header() {
                                       sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full"
                                 initial={{ y: -100, x: "-50%", opacity: 0 }}
                                 animate={{ y: 0, x: "-50%", opacity: 1 }}></motion.div>
+
+            <nav className="fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem]
+                                sm:h-[initial] sm:py-0">
+                
+            </nav>
         </header>
     );
 }
