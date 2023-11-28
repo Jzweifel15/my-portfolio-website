d336f7c0c83fe595e9d77f9034b8c4a116843e72
Justin R. Zweifel <jzweifel15@yahoo.com>
2023-11-28 02:21:36 -0500

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -4,6 +4,10 @@ import Image from 'next/image';
 import React from 'react';
 import Me from "@/public/Me.jpeg";
 import { motion } from "framer-motion";
+import Link from 'next/link';
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { FaGithubSquare } from "react-icons/fa";
 
 export default function Intro() {
   return (
@@ -30,11 +34,45 @@ export default function Intro() {
                 </motion.span>
             </div>
         </div>
-        <p className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl">
+        <motion.p className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+                    initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0 }}>
             <span className="font-bold">Hello, I'm Justin.</span> I'm an{" "}
             <span className="font-bold">aspiring software development engineer</span>. 
             I enjoy buliding <span className="italic"> full-stack sites & apps</span>. 
-        </p>
+        </motion.p>
+
+        <motion.div className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+                        initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0}} 
+                        transition={{ delay: 0.1 }}>
+
+            <Link href="#contact" className="group bg-gray-900 text-white px-7 py-3 flex items-center 
+                                                gap-2 rounded-full outline-none focus:scale-110
+                                                hover:scale-110 hover:bg-gray-950 active:scale-105
+                                                transition">
+                Contact me here <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+            </Link>
+
+            <a href="/Resume.docx" download className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full 
+                                        outline-none focus:scale-110 hover:scale-110 active:scale-105 transition 
+                                        cursor-pointer border border-black/10">
+                Download Resume <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+            </a>
+
+            <a href="https://www.linkedin.com/in/justin-zweifel-3554511aa" target="_blank" 
+                    className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full 
+                                 focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 
+                                 active:scale-105 transition cursor-pointer border border-black/10">
+                <BsLinkedin />
+            </a>
+
+            <a href="https://github.com/Jzweifel15" target="_blank" 
+                    className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] 
+                                 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 
+                                 active:scale-105 transition cursor-pointer border border-black/10">
+                <FaGithubSquare />
+            </a>
+
+        </motion.div>
     </section>
   );
 }
