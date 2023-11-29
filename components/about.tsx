be35feedd6e12ae6f385d41a5ea1cb62c298bb45
Justin R. Zweifel <jzweifel15@yahoo.com>
2023-11-29 00:37:05 -0500

    Update about.tsx

--- components/about.tsx
+++ components/about.tsx
@@ -6,9 +6,10 @@ import { motion } from "framer-motion";
 
 export default function About() {
   return (
-    <motion.section className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40"
+    <motion.section className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
                         initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0 }}
-                        transition={{ delay: 0.175 }}>
+                        transition={{ delay: 0.175 }}
+                        id="about">
         <SectionHeading>About Me</SectionHeading>
         <p className="mb-3">
             Currently, I am an IT Support Technician II for Amazon and I'm looking to get into a{" "} 
