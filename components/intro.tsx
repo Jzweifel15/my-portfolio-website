bbc8f8344ae0f17f23b1faac5791b35187b50023
Justin R. Zweifel <47803568+Jzweifel15@users.noreply.github.com>
2023-11-29 22:21:03 -0500

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -4,16 +4,13 @@ import Image from "next/image";
 import React from "react";
 import Me from "@/public/Me.jpeg";
 import { motion } from "framer-motion";
-import Link from "next/link";
-import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { BsLinkedin } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
 import { useSectionInView } from "@/lib/hooks";
-import { useActiveSectionContext } from "@/context/active-section-context";
 
 export default function Intro() {
   const { ref } = useSectionInView("Home", 0.5);
-  const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
 
   return (
     <section ref={ ref } id="home" className="mb-28 max-w-[50rem] text-center 
@@ -49,21 +46,13 @@ export default function Intro() {
                              gap-2 px-4 text-lg font-medium"
                   initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0 }}
                   transition={{ delay: 0.1 }}>
-        <Link href="#contact" className="group bg-gray-900 text-white px-7 py-3 flex items-center 
+
+        <a className="group bg-gray-900 text-white px-7 py-3 flex items-center 
                                          gap-2 rounded-full outline-none focus:scale-110 hover:scale-110
                                          hover:bg-gray-950 active:scale-105 transition"
-              onClick={() => {
-                setActiveSection("Contact");
-                setTimeOfLastClick(Date.now());
-              }}>
-          Contact me here <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
-        </Link>
-
-        <a className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none 
-                      focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer 
-                      borderBlack dark:bg-white/10"
-          href="/CV.pdf" download>
-          Download Resume <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+          href="https://docs.google.com/document/d/1IS-c6YvgnADZJ6kB8mqlcSF7aTDSvjFr/edit?usp=sharing&ouid=113997322441545737596&rtpof=true&sd=true" 
+          target="_blank">
+          Resume <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
         </a>
         <a className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 
                       rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition 
